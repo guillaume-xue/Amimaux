e6c2b5c97d193a05331abfffe210873ef54a1060
XiaoGunFr <davidyu198000507@gmail.com>
2024-11-13 12:46:26 +0100

    Initial commit

---
+++ app/src/test/java/ufr/m1/prog_mobile/projet/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package ufr.m1.prog_mobile.projet
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
